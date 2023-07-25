7a03f6b1a0b16a0b51ff59f1e9f809962888378c
yhaw12 <yooxing11@gmail.com>
2023-07-25 00:12:54 +0000

    update

--- src/components/signin/Signin.jsx
+++ src/components/signin/Signin.jsx
@@ -4,51 +4,52 @@ import './signin.css'
 import { useState } from 'react'
 import { FaEnvelope } from 'react-icons/fa'
 import { FaEye } from 'react-icons/fa'
+import { FaGoogle } from 'react-icons/fa'
 
 import { GoogleAuthProvider } from "firebase/auth";
 
 // const provider = new GoogleAuthProvider();
 
-import { getAuth } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
 
-const auth = getAuth();
-auth.languageCode = 'it';
+// const auth = getAuth();
+// auth.languageCode = 'it';
 
-import { getAuth, signInWithRedirect } from "firebase/auth";
+// import { getAuth, signInWithRedirect } from "firebase/auth";
 
-const auth = getAuth();
-signInWithRedirect(auth, provider);
+// const auth = getAuth();
+// signInWithRedirect(auth, provider);
 
 
 // To apply the default browser preference instead of explicitly setting it.
 // auth.useDeviceLanguage();
-import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
+// import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
 
-const auth = getAuth();
-getRedirectResult(auth)
-  .then((result) => {
+// const auth = getAuth();
+// getRedirectResult(auth)
+//   .then((result) => {
     // This gives you a Google Access Token. You can use it to access Google APIs.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
+    // const credential = GoogleAuthProvider.credentialFromResult(result);
+    // const token = credential.accessToken;
 
     // The signed-in user info.
-    const user = result.user;
+    // const user = result.user;
     // IdP data available using getAdditionalUserInfo(result)
     // ...
-  }).catch((error) => {
+//   }).catch((error) => {
     // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
+    // const errorCode = error.code;
+    // const errorMessage = error.message;
     // The email of the user's account used.
-    const email = error.customData.email;
+    // const email = error.customData.email;
     // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
+    // const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
-  });
+//   });
 
 function Signin() {
 
-    const[changePassword, setchangePassword] = useState(false)
+    // const[changePassword, setchangePassword] = useState(false)
   return (
     <div className='singin_main_card'>
         <div className='signin_ac_card'>
@@ -61,10 +62,18 @@ function Signin() {
                 <div className="login_card"><input type="email" placeholder='Email' /><FaEnvelope/></div>
                 <div className="login_card"><input type="text" placeholder='Password'/><FaEye/></div>
 
-                <h5>Forgot your password?</h5>
+                <div className="txt"><h5>Forgot password?</h5>
+                <h5>Don't have an account</h5></div>
+
+                
              <button type="submit" className='btn'>login</button>
-             https://website-design-ae9ea.firebaseapp.com/__/auth/handler
+             {/* https://website-design-ae9ea.firebaseapp.com/__/auth/handler */}
             </form>
+            <div className='sp'><span>OR</span></div>
+            
+            <div className="bt"><FaGoogle/><button type="submit" className='btn'>Continue with Google</button></div>
+
+            
 
         </div>
     </div>
